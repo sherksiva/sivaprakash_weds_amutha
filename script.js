0565f173103816faa606223f1ea4c9dadde7334b
sherksiva <prakashraman441@gmail.com>
2021-11-08 13:37:12 +0530

    Code simplify

--- script.js
+++ script.js
@@ -8,43 +8,10 @@
 
 $(document).ready(function () {
 
-    // Set the date we're counting down to
-    // var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
-
-    // Update the count down every 1 second
-    // var x = setInterval(function() {
-
-    //     // Get todays date and time
-    //     var now = new Date().getTime();
-        
-    //     // Find the distance between now and the count down date
-    //     var distance = countDownDate - now;
-        
-    //     // Time calculations for days, hours, minutes and seconds
-    //     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    //     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    //     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-    //     // Output the result in an element with id="demo"
-    //     document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-        
-    //     // If the count down is over, write some text 
-    //     // if (distance < 0) {
-    //     //     clearInterval(x);
-    //     //     document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
-    //     // }
-    // }, 1000);
-    // document.getElementById("my_audio").play();
-    $(document).on('click', function(){
-        // document.getElementById("my_audio").play();
-        console.log('Shaadi me zaroor aana');
-    });
-    if(document.getElementById("my_audio")) {
-        document.getElementById("my_audio").play();
-    
-        }
-    // being a bit cool :p  
+    // $(document).on('click', function(){
+    //     document.getElementById("my_audio").play();
+    // });
+ 
     var styles = [
         'background: linear-gradient(#D33106, #571402)'
         , 'border: 4px solid #3E0E02'
